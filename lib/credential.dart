c7d38f7774f87ab186b39372db0432a48781981c
vit con lon ton <hoanghiep97@hotmail.com>
2022-10-08 19:29:47 +0700

    Improve: add default email, password compare in login

---
+++ lib/credential.dart
@@ -0,0 +1,4 @@
+import 'package:mvvm_test/domain/value_objects/value_objects.dart';
+
+final Password myPassword = Password('123456');
+final EmailAddress myEmailAddress = EmailAddress('hello@example.com');
